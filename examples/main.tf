02898565b418c5360418d17121863dab10d274b8
trentmillar <1146672+trentmillar@users.noreply.github.com>
2023-03-22 09:12:01 -0600

    fix example main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,4 +1,13 @@
-provider "git" {
+terraform {
+  required_providers {
+    git = {
+      versions = ["0.1"]
+      source = "github.com/go-pax/git"
+    }
+  }
+}
+
+provider git {
   owner = "test-dump"
   token = ""
 }
